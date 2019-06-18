67d51e2fb9d7b82a0e81250dce19e6be5ffe3283
Scr@psBits <scrapsbits@gmail.com>
2019-06-18 13:58:42 +0200

    Added package "controllers" to house controllers.

--- Java/src/main/ui/controllers/ProjectCreatorController.java
+++ Java/src/main/ui/controllers/ProjectCreatorController.java
@@ -1,6 +1,6 @@
-package main.ui;
+package main.ui.controllers;
 
-public class Controller {
+public class ProjectCreatorController {
 	public void initialize() {
 		String javaVersion = System.getProperty("java.version");
 		String javafxVersion = System.getProperty("javafx.version");
