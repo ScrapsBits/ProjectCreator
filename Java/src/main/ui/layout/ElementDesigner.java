1f902546679ce09e9321004af52b165fb4f3334c
Scr@psBits <scrapsbits@gmail.com>
2019-06-21 11:13:05 +0200

    UI design.
    Added ElementDesigner class to position elements on the UI.

---
+++ Java/src/main/ui/layout/ElementDesigner.java
@@ -0,0 +1,23 @@
+package main.ui.layout;
+
+import javafx.scene.Scene;
+
+/**
+ * A class to design the single-view user interface.
+ * @author ScrapsBits
+ *
+ */
+public final class ElementDesigner {
+	/**
+	 * The scene displayed by the user interface.
+	 */
+	private Scene scene;
+	
+	/**
+	 * Instantiate the element designer class.
+	 * @param scene The scene displayed by the user interface.
+	 */
+	public ElementDesigner(Scene scene) {
+		this.scene = scene;
+	}
+}
